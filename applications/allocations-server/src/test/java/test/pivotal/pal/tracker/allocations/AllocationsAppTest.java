d4285ea3330422eea85646c21ab68f47272cbbf8
Nageswara <nageswara.dachiraju@cognizant.com>
2020-05-04 11:16:04 -0500

    ServicesDiscoveryLab

--- applications/allocations-server/src/test/java/test/pivotal/pal/tracker/allocations/AllocationsAppTest.java
+++ applications/allocations-server/src/test/java/test/pivotal/pal/tracker/allocations/AllocationsAppTest.java
@@ -11,9 +11,7 @@ public class AllocationsAppTest {
     @Test
     public void embedded() {
         App.main(new String[]{});
-
         String response = new RestTemplate().getForObject("http://localhost:8181/allocations?projectId=0", String.class);
-
         assertThat(response).isEqualTo("[]");
     }
 }
